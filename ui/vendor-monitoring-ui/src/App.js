d2e5ad01ae1fb5b40e6c36e540e02ba1a52905cd
jackjduggan <20094010@mail.wit.ie>
2024-09-19 15:11:21 +0100

    set interval of 60s betweeen fetches, appears to be working!

--- ui/vendor-monitoring-ui/src/App.js
+++ ui/vendor-monitoring-ui/src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
 
-  const [google_status, set_google_status] = useState()
-  const [amazon_status, set_amazon_status] = useState()
   const [status, setStatus] = useState([])
 
-  useEffect(() => {
+  const fetchStatus = async () => {
     axios.get('http://localhost:5000/v1/all-status')
     .then(response => {
       setStatus(response.data);
@@ -17,17 +14,26 @@ function App() {
     .catch(error => {
       console.error(error);
     })
-  })
+  }
+
+  useEffect(() => {
+    fetchStatus(); // initial fetch
+
+    const intervalId = setInterval(fetchStatus, 60000); // fetch every 60 seconds
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div>
+      <h1>Vendor Monitoring Screen</h1>
       <ul>
         {status.map(item => (
           <li>
-            <strong>URL:</strong> {item.url}<br/>
-            <strong>Status Code:</strong> {item.statusCode}<br/>
-            <strong>Duration:</strong> {item.duration} ms<br/>
-            <strong>Date:</strong> {item.date}<br/>
+            <h3>{item.url}</h3>
+            <strong>Status Code:</strong> {item.statusCode}<br></br>
+            <strong>Duration:</strong> {item.duration} ms<br></br>
+            <strong>Date:</strong> {item.date}<br></br>
           </li>
         ))}
       </ul>
