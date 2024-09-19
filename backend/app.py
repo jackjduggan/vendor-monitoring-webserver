1b3530f3ae9656c52c752dd5073a5df7df54beb1
jackjduggan <20094010@mail.wit.ie>
2024-09-19 13:57:21 +0100

    pip freezed into requirements.txt, Flask skeleton created

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,23 @@
 # TODO: Flask application that uses python requests to make http GET requests to amazon and google. Endpoints will then be created for these checks.
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+import requests
+import time
+
+app = Flask(__name__)
+
+def status_check(url):
+    pass
+
+def amazon_status():
+    pass
+
+def amazon_status():
+    pass
+
+def amazon_status():
+    pass
+
+if __name__ == "__main__":
+    app.run()
