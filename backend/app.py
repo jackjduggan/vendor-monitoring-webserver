f2fba51a9ff39cd3518bc86da862e311e77e2bf8
jackjduggan <20094010@mail.wit.ie>
2024-09-19 14:19:32 +0100

    Amazon 503 error solved: 'User-Agent' header required

--- backend/app.py
+++ backend/app.py
@@ -9,12 +9,20 @@ app = Flask(__name__)
 
 def status_check(url):
     start_time = time.time()
-    response = requests.get(url)
+
+    # 503 Error Solution - Amazon requires that you specify 'User-Agent' header in order to return 200 response
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0'} # https://stackoverflow.com/questions/64192137/amazon-com-returns-status-503
+
+    response = requests.get(
+        url=url, 
+        headers=headers
+        )
+    
     return {
         "url": url,
         "statusCode": response.status_code,
-        "duration": int((time.time() - start_time)*1000),
-        "date": int(time.time()) # as an int?
+        "duration": int((time.time() - start_time)*1000), # (current time - start time) to calculate duration
+        "date": int(time.time())
     }
 
 @app.route('/v1/amazon-status', methods=['GET'])
